96a651dd1010598113cd0f42ca771403881c6ad9
Andreas Jaeger <aj@suse.com>
2019-12-18 09:51:59 +0100

    Retire repository
    Fuel (from openstack namespace) and fuel-ccp (in x namespace)
    repositories are unused and ready to retire.
    
    This change removes all content from the repository and adds the usual
    README file to point out that the repository is retired following the
    process from
    https://docs.openstack.org/infra/manual/drivers.html#retiring-a-project
    
    See also
    http://lists.openstack.org/pipermail/openstack-discuss/2019-December/011647.html
    
    Depends-On: https://review.opendev.org/699362
    Change-Id: I9227181bae1ac7c39f1dd714d31e9d23e8dbc969

--- contrib/README.md
+++
@@ -1,4 +0,0 @@
-contrib
-=======
-
-This directory used for some useful scripts/additions used mostly for debugging purposes.
--- contrib/osx-nat-pf/osx-nat-pf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sudo pfctl -f ./osx-nat-pf.rules -e # starts pfctl and loads the rules from the osx-nat-pf.rules file
-sudo sysctl net.inet.ip.forwarding=1 # enabe routing mode
-
